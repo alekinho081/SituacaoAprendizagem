ee87484081f51b6f184230d221070a0b08473439
alekinho081 <alexborbajr0@gmail.com>
2025-06-25 17:18:06 -0300

    adicionando elemento layout para deixar um padrão de estilo

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,33 @@ import Home from './Pages/HomePage/Home';
 import Cadastro from './Pages/CadastroPage/Cadastro';
 import Login from './Pages/LoginPage/Login';
 import Consulta from './Pages/ConsultaPage/Consulta';
+import Layout from './layout/Layout';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-            <Route path='/' element={<Home/>} />
-            <Route path='/cadastro' element={<Cadastro/>} />
-            <Route path='/login' element={<Login/>} />
-            <Route path='/consulta' element={<Consulta/>} />
+          <Route path='/' element={<Layout />} >
+
+            <Route index element={< Home />} />
+
+            
+            <Route path='/consulta' element={<Consulta />} />
+
+          </Route>
+
+
+          <Route path='/cadastro' element={<Cadastro />} />
+          <Route path='/login' element={<Login />} />
+
+
+
+
         </Routes>
       </BrowserRouter>
     </div>
   )
 }
- 
+
 export default App
--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -4,7 +4,6 @@ import Typography from "@mui/material/Typography";
 const Home = () => {
     return (
         <div>
-            <NewHeader />
             <div style={{ padding: '20px' }}>
                 <Typography variant="body1">Inicio</Typography>
             </div>
